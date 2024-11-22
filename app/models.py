d5c3939cb27e7b11a0201e6730f932fa89e21ae5
F.Sachin Albert <sachinalbert1@gmail.com>
2024-11-22 11:45:10 +0530

    Modified

--- app/models.py
+++ app/models.py
@@ -4,3 +4,6 @@ class createdata(models.Model):
     name = models.CharField(max_length=200,null=False)
     phone = models.IntegerField()
     para = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return self.name
